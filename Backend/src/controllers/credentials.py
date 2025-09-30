bd61a492a84a1b6e032830c5b57f05842ea279e0
dani058410 <danel0504102564@gmail.com>
2025-09-30 14:58:20 +0300

    solved merged conflict

--- Backend/src/controllers/credentials.py
+++ Backend/src/controllers/credentials.py
@@ -4,32 +4,18 @@ from src.services.credentials import CredentialsService
 class CredentialsController:
     
     @staticmethod
-<<<<<<< Updated upstream
+
     def add_device_cred(cred):
         CredentialsService.add_device_cred(cred)
-=======
-    def add_device(cred):
-        CredentialsService.add_device(cred)
->>>>>>> Stashed changes
-        
+
 
     @staticmethod
     def get_all_cred():
         return CredentialsService.get_all_cred()
-<<<<<<< Updated upstream
-
-    @staticmethod
-    def get_one_cred(ip):
-        return CredentialsService.get_one_cred(ip)
-=======
 
 
     @staticmethod
     def get_one_cred(ip):
         return CredentialsService.get_one_cred(ip)
-    
-    
-  
 
     
->>>>>>> Stashed changes
