c86954e02db37ccb761ecaa11e9bbafe44acdc04
RudeusTempest <yosefkaro2003@gmail.com>
2025-11-24 11:58:44 +0200

    added a route to start program

--- Backend/src/controllers/devices.py
+++ Backend/src/controllers/devices.py
@@ -31,7 +31,20 @@ class DeviceController:
             await DeviceService.update_mbps_snmp()
             await asyncio.sleep(mbps_interval)
     
-             
+
+    @staticmethod
+    async def main_snmp(device_interval, mbps_interval) -> None:
+        try:
+            while True:
+                await asyncio.gather(
+                    DeviceController.periodic_refresh_snmp(device_interval), 
+                    DeviceController.update_mbps_loop_snmp(mbps_interval)
+                )
+        except Exception as e:
+            print(f"Error in main_snmp: {e}")
+            raise
+
+
 
 
 #""""""""""""""""""""""""""""""""""""""""""""""""""CLI METHODES""""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -39,3 +52,16 @@ class DeviceController:
     @staticmethod
     async def periodic_refresh_cli(device_interval: float) -> None:
         await DeviceService.periodic_refresh_cli(device_interval)
+
+
+    @staticmethod
+    async def main_cli(device_interval, mbps_interval) -> None:
+        try:
+            while True:
+                await asyncio.gather(
+                    DeviceController.periodic_refresh_cli(device_interval), 
+                    DeviceService.update_mbps_loop_cli(mbps_interval)
+                )
+        except Exception as e:
+            print(f"Error in main_cli: {e}")
+            raise    
